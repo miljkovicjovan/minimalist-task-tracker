32f15f491f569d8ec2cb8d96bbd4bdcf5fc502bc
Jovan Miljkovic <jovan.miljkovic.biz@gmail.com>
2023-10-22 17:30:53 +0200

    separated footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <span>
+        by <a href="https://github.com/miljkovicjovan" target='_blank'  rel="noreferrer">
+        @miljkovicjovan
+        </a>
+    </span>
+  )
+}
+
+export default Footer
