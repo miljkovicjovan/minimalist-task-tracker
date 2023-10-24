67fe5c0560a9b93d6313dcb732fcce0a71a0fbb9
Jovan Miljkovic <77690201+miljkovicjovan@users.noreply.github.com>
2023-10-24 03:00:55 +0200

    finished, total, percentage added

--- src/components/FinishedTasks.jsx
+++ src/components/FinishedTasks.jsx
@@ -1,11 +1,13 @@
 import { Stack } from "react-bootstrap";
 
-function FinishedTasks({ finishedTasks}) {
+function FinishedTasks({ finishedTasks, tasks }) {
   return (
     <Stack className="text-center pt-4">
         <h4>Finished Tasks &#129321;</h4>
         <p className="text-secondary">
-          Finished:{finishedTasks.length}
+          {`Finished: ${finishedTasks.length} `}
+          {`- Total: ${finishedTasks.length+tasks.length} `}
+          {`- Percentage: ${Math.round((finishedTasks.length / (finishedTasks.length+tasks.length)) * 100)}% `}
         </p>       
         {finishedTasks.map((finishedTask, index) => (
           <span key={index}>&#x2705; {finishedTask}</span>
