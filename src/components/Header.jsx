d0362b74fec2675e0c4eca4101b91db36e699ecf
Jovan Miljkovic <77690201+miljkovicjovan@users.noreply.github.com>
2023-11-03 02:36:23 +0100

    fixed horizontal allignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,21 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <Nav className="pt-4 d-flex justify-content-center">
-      <Nav.Item>
+    <Nav className="pt-4 d-flex justify-content-center align-items-center">
+      {/* This item is hidden in order to make the Logo be centered horizontally */}
+      <Nav.Item style={{ visibility: 'hidden' }}>
+        <Link to="/archive">
+          Archived Tasks
+        </Link>
+      </Nav.Item>
+      <Nav.Item className="mx-5">
         <Link to="/">
           <img draggable="false" loading="eager" width="245" src={LOGO} alt="logo" />
         </Link>
       </Nav.Item>
-      <Nav.Item>
+      <Nav.Item className="nav-link m-0 p-0">
         <Link to="/archive">
-          Archive
+          Archived Tasks
         </Link>
       </Nav.Item>
     </Nav>
