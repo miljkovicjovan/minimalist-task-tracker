bf579667e8f42550fb8e2028a08fc73724be828c
Jovan Miljkovic <77690201+miljkovicjovan@users.noreply.github.com>
2023-11-03 12:17:06 +0100

    confirmation modal when archiving task

--- src/components/FinishedTasks.jsx
+++ src/components/FinishedTasks.jsx
@@ -1,13 +1,23 @@
 import { faTrashCan, faBoxArchive } from "@fortawesome/free-solid-svg-icons";
-import { Stack, Button } from "react-bootstrap";
+import { Stack, Button, Modal } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 function FinishedTasks({ finishedTasks, tasks, onReset, onArchive }) {
   const [hover, setHover] = useState(false);
   const toggleHover = () => setHover(!hover);
+
+  const [show, setShow] = useState(false);
+  const [selectedTask, setSelectedTask] = useState();
+  const handleClose = () => setShow(false);
+  const handleShow = (id) => {
+    setShow(true); 
+    setSelectedTask(id);
+  }
+
   return (
-    <Stack className="text-center pt-4">
+    <>
+      <Stack className="text-center pt-4">
         <hr className="mx-auto w-25"/>
         <h4 className="text-decoration-underline">Finished Tasks &#129321;</h4>
         <p className="text-secondary">
@@ -19,7 +29,7 @@ function FinishedTasks({ finishedTasks, tasks, onReset, onArchive }) {
           return finishedTask.archived === false ?
             <span className="my-1">
               <span key={index}>&#x2705; {finishedTask.name}</span>
-              <Button className="ms-2" size="sm" onClick={() => onArchive(finishedTask.id)}>
+              <Button className="ms-2" size="sm" onClick={() => handleShow(finishedTask.id)}>
                 <FontAwesomeIcon icon={faBoxArchive} className='pe-1' />
                 Archive Task
               </Button>
@@ -38,7 +48,25 @@ function FinishedTasks({ finishedTasks, tasks, onReset, onArchive }) {
               Delete Finished Tasks
           </Button>
         </span> 
-    </Stack>
+      </Stack>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to archive this task?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={() => {
+            onArchive(selectedTask);
+            handleClose();
+          }}>
+            I'm sure
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
 
