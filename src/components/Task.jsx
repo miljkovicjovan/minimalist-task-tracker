c198cf45e8fcf5dce1adb917addde36ef8a091e2
Nguyen Phu Cuong <littlecuong922@gmail.com>
2023-11-03 15:04:06 +0700

    Disable finish & delete button when task is in editing mode

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,10 @@
 import { Button, Stack, Modal } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faPenToSquare,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { Tooltip } from "react-tooltip";
 
@@ -21,69 +25,96 @@ function Task({ id, index, name, onDelete, onEdit, onComplete }) {
 
   return (
     <>
-      <Stack 
-        direction="horizontal" 
+      <Stack
+        direction="horizontal"
         className="task m-2 mx-auto border rounded d-flex justify-content-between"
       >
-        {!editMode ? <span className="p-2">#{index+1} {name}</span> :
-        <span className="d-flex align-items-center">
-          <span className="text-secondary ps-2 pe-1"><i>Edit Mode</i></span>
-          <form onSubmit={(e) => {
-            e.preventDefault();
-            onEdit(editedName, id);
-            setEditMode(false);
-          }}>
-            <input 
-              type="text" 
-              value={editedName}
-              placeholder="Add a Task"
-              className='bg-dark text-light rounded p-1 border-0'
-              onChange={(e) => setEditedName(e.target.value)}
-            />
-          </form>        
-        </span>}
+        {!editMode ? (
+          <span className="p-2">
+            #{index + 1} {name}
+          </span>
+        ) : (
+          <span className="d-flex align-items-center">
+            <span className="text-secondary ps-2 pe-1">
+              <i>Edit Mode</i>
+            </span>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                onEdit(editedName, id);
+                setEditMode(false);
+              }}
+            >
+              <input
+                type="text"
+                value={editedName}
+                placeholder="Add a Task"
+                className="bg-dark text-light rounded p-1 border-0"
+                onChange={(e) => setEditedName(e.target.value)}
+              />
+            </form>
+          </span>
+        )}
         <span>
-          <Button 
+          <Button
             className={`border-success border-2 rounded-0 finish-task 
-            ${finishHover ? "bg-light border-light text-success" : "bg-success text-light"}`}
+            ${
+              finishHover
+                ? "bg-light border-light text-success"
+                : "bg-success text-light"
+            }
+            ${editMode && "bg-dark text-light"}
+            `}
+            disabled={editMode && true}
             onMouseEnter={toggleHoverFinish}
             onMouseLeave={toggleHoverFinish}
             onClick={() => onComplete(name, id)}
           >
-            <FontAwesomeIcon icon={faCheck}/>
+            <FontAwesomeIcon icon={faCheck} />
           </Button>
           <Tooltip
-              className="d-none d-lg-block"
-              anchorSelect=".finish-task"
-              content="Finish Task"
+            className="d-none d-lg-block"
+            anchorSelect=".finish-task"
+            content="Finish Task"
           />
-          <Button 
+          <Button
             className={`border-primary border-2 rounded-0 edit-task 
-            ${editHover ? "bg-light border-light text-primary" : "bg-primary text-light"}`}
+            ${
+              editHover
+                ? "bg-light border-light text-primary"
+                : "bg-primary text-light"
+            }`}
             onMouseEnter={toggleHoverEdit}
             onMouseLeave={toggleHoverEdit}
             onClick={() => setEditMode(!editMode)}
           >
-            <FontAwesomeIcon icon={faPenToSquare}/>
+            <FontAwesomeIcon icon={faPenToSquare} />
           </Button>
           <Tooltip
-              className="d-none d-lg-block"
-              anchorSelect=".edit-task"
-              content="Edit Task"
+            className="d-none d-lg-block"
+            anchorSelect=".edit-task"
+            content="Edit Task"
           />
-          <Button 
+          <Button
             className={`border-danger border-2 rounded-0 rounded-end delete-task 
-            ${deleteHover ? "bg-light border-light text-danger" : "bg-danger text-light"}`}
+            ${
+              deleteHover
+                ? "bg-light border-light text-danger"
+                : "bg-danger text-light"
+            }
+            ${editMode && "bg-dark text-light"}
+            `}
+            disabled={editMode && true}
             onMouseEnter={toggleHoverDelete}
             onMouseLeave={toggleHoverDelete}
             onClick={handleShow}
           >
-            <FontAwesomeIcon icon={faTrash}/>
+            <FontAwesomeIcon icon={faTrash} />
           </Button>
           <Tooltip
-              className="d-none d-lg-block"
-              anchorSelect=".delete-task"
-              content="Delete Task"
+            className="d-none d-lg-block"
+            anchorSelect=".delete-task"
+            content="Delete Task"
           />
         </span>
       </Stack>
@@ -91,22 +122,28 @@ function Task({ id, index, name, onDelete, onEdit, onComplete }) {
         <Modal.Header closeButton>
           <Modal.Title>Are you sure?</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Are you sure you want to delete this task? 
-          <br/>Once you delete a task you cannot get it back!</Modal.Body>
+        <Modal.Body>
+          Are you sure you want to delete this task?
+          <br />
+          Once you delete a task you cannot get it back!
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="danger" onClick={() => {
-            handleClose();
-            onDelete(id);
-          }}>
+          <Button
+            variant="danger"
+            onClick={() => {
+              handleClose();
+              onDelete(id);
+            }}
+          >
             I'm sure
           </Button>
         </Modal.Footer>
       </Modal>
     </>
-  )
+  );
 }
 
 export default Task;
