50fac7eec4348e4d428922d56bc1e4d182f9697b
Jovan Miljkovic <77690201+miljkovicjovan@users.noreply.github.com>
2023-11-04 03:09:43 +0100

    #24 Add repository stars to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,27 @@
+import GitHubButton from 'react-github-btn';
+
 function Footer() {
   return (
-    <footer className="text-center py-3">
+    <footer className="py-3 d-flex justify-content-center align-items-center">
       Contribute or support us on{" "}
       <a
         href="https://github.com/miljkovicjovan/minimalist-task-tracker"
         target="_blank"
         rel="noreferrer"
-        className="text-primary text-decoration-none"
+        className="text-primary text-decoration-none mx-2"
       >
         GitHub &#10084;&#65039;
       </a>
+      <GitHubButton
+        href="https://github.com/miljkovicjovan/minimalist-task-tracker"
+        data-icon="octicon-star"
+        data-size="large"
+        data-show-count="true"
+        aria-label="Star miljkovicjovan/minimalist-task-tracker on GitHub"
+        className="p-0 m-0"
+      >
+        Give us a Star
+      </GitHubButton>
     </footer>
   );
 }
